6c7b7f398538b93edd0ace565b4d730eaca82233
MrssiP <mrssip@foxmail.com>
2018-01-04 11:30:37 +0800

    校友会模块

--- Public/js/admin/common.js
+++ Public/js/admin/common.js
@@ -11,23 +11,42 @@ $("#button-add").click(function(){
  */
 $("#singcms-button-submit").click(function(){
     var data = $("#singcms-form").serializeArray();
-    postData = {};
+    var postData = {};
     $(data).each(function(i){
        postData[this.name] = this.value;
     });
     console.log(postData);
     // 将获取到的数据post给服务器
-    url = SCOPE.save_url;
-    jump_url = SCOPE.jump_url;
-    $.post(url,postData,function(result){
-        if(result.status == 1) {
-            //成功
-            return dialog.success(result.message,jump_url);
-        }else if(result.status == 0) {
-            // 失败
-            return dialog.error(result.message);
-        }
-    },"JSON");
+    // url = SCOPE.save_url;
+    // jump_url = SCOPE.jump_url;
+    // $.post(url,postData,function(result){
+    //     if(result.status == 1) {
+    //         //成功
+    //         return dialog.success(result.message,jump_url);
+    //     }else if(result.status == 0) {
+    //         // 失败
+    //         return dialog.error(result.message);
+    //     }
+    // },"JSON");
+
+    // 通过Ajax方法
+    $.ajax({
+        type: "POST",
+        url: SCOPE.save_url,
+        dataType: "json",
+        data: postData,
+        success: function(result){
+            if(result.status == 1) {
+                //成功
+                return dialog.success(result.message,SCOPE.jump_url);
+            }else if(result.status == 0) {
+                // 失败
+                return dialog.error(result.message);
+            }
+        },
+        error: function(){
+            alert("网络连接错误！");
+    }});
 });
 /*
 编辑模型
