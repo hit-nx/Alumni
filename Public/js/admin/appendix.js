4390cfc3148a000bbd831ef12b85ecdc065f12f6
LRyan <1666166800@qq.com>
2018-01-20 00:01:52 +0800

    为了pull

--- Public/js/admin/appendix.js
+++ Public/js/admin/appendix.js
@@ -17,7 +17,6 @@ $(function() {
 
                 console.log(data);
                 $('#' + file.id).find('.data').html(' 上传完毕');
-
                 $("#upload_org_code_img").attr("src",obj.data);
                 $("#file_upload_image").attr('value',obj.data);
 
