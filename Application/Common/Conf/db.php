5afc5c589b7a1cb4413cf002e8d2e527f3fa1bdf
zzw <905162195@qq.com>
2018-03-05 22:41:42 +0800

    Revert "Merge branch 'master' of https://github.com/hit-nx/Alumni"
    This reverts commit 640e1b732a3e5b1cf1404323a0ef749bf4588afc, reversing
    changes made to 09d6fdac32ac5996da5bfc8f4629861d5318fa72.

--- Application/Common/Conf/db.php
+++ Application/Common/Conf/db.php
@@ -15,15 +15,7 @@ return array(
    'DB_TYPE' => 'mysql',
    'DB_HOST' => '127.0.0.1',
    'DB_USER' => 'root',
-<<<<<<< HEAD
    'DB_PWD' => 'lamessi10',
-=======
-<<<<<<< HEAD
-   'DB_PWD' => '19970414',
-=======
-   'DB_PWD' => '123456',
->>>>>>> fddb912665bafcf2453395c95cb47412de8413b1
->>>>>>> d23fb074b2ebcefe0a383eb83f8408bc24dad839
    'DB_PORT' => 3306,
    'DB_NAME' => 'alumni',
    'DB_CHARSET' => 'utf8',
