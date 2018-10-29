be9bb118ae56be505d8cbaba33976ce8a6860698
MrssiP <mrssip@foxmail.com>
2018-10-29 21:15:58 +0800

    数据库配置文件
    local -> aliyun

--- Application/Common/Conf/db.php
+++ Application/Common/Conf/db.php
@@ -1,23 +1,23 @@
 <?php
 return array(
 
-    // 'DB_TYPE' => 'mysql',
-    // 'DB_HOST' => '47.94.99.203',
-    // 'DB_USER' => 'root',
-    // 'DB_PWD' => 'ningximysql',
-    // 'DB_PORT' => 3306,
-    // 'DB_NAME' => 'alumni',
-    // 'DB_CHARSET' => 'utf8',
-    // 'DB_PREFIX' =>'alumni_',
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => '39.106.1.159',
+    'DB_USER' => 'root',
+    'DB_PWD' => 'mrssip10',
+    'DB_PORT' => 3306,
+    'DB_NAME' => 'alumni',
+    'DB_CHARSET' => 'utf8',
+    'DB_PREFIX' =>'alumni_',
 
 
     // 本地数据库
-   'DB_TYPE' => 'mysql',
-   'DB_HOST' => '127.0.0.1',
-   'DB_USER' => 'root',
-   'DB_PWD' => 'lamessi10',
-   'DB_PORT' => 3306,
-   'DB_NAME' => 'alumni',
-   'DB_CHARSET' => 'utf8',
-   'DB_PREFIX' =>'alumni_',
+   // 'DB_TYPE' => 'mysql',
+   // 'DB_HOST' => '127.0.0.1',
+   // 'DB_USER' => 'root',
+   // 'DB_PWD' => '',
+   // 'DB_PORT' => 3306,
+   // 'DB_NAME' => 'alumni',
+   // 'DB_CHARSET' => 'utf8',
+   // 'DB_PREFIX' =>'alumni_',
 );
